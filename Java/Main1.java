32006bd53079f3fe110c14d454a88f1bf1d42cb9
Pedro <pedromiguel-99@hotmail.com>
2021-03-19 11:53:35 +0000

    Corrected errors in tests

--- Java/Main1.java
+++ Java/Main1.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.SocketException;
 import java.util.ArrayList;
 
 public class Main1 {
--- Java/Receiver.java
+++ Java/Receiver.java
@@ -4,7 +4,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 
-public class Receiver {
+public class Receiver implements Runnable {
 
     private DatagramSocket socket;
     private boolean running;
@@ -14,25 +14,22 @@ public class Receiver {
         socket = new DatagramSocket(10000);
     }
 
-    public void run() throws IOException {
+    public void run() {
         running = true;
 
         while (running) {
             DatagramPacket packet
                     = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             InetAddress address = packet.getAddress();
             int port = packet.getPort();
-            packet = new DatagramPacket(buf, buf.length, address, port);
-            String received
-                    = new String(packet.getData(), 0, packet.getLength());
 
-            if (received.equals("end")) {
-                running = false;
-                continue;
-            }
-            socket.send(packet);
+            System.out.println("Chegou qq coisa");
         }
         socket.close();
     }
