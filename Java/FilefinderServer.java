923cc3311a65d4830d2e102656762af41cf91532
Pedro <pedromiguel-99@hotmail.com>
2021-04-14 15:46:47 +0100

    debugging code

--- Java/FilefinderServer.java
+++ Java/FilefinderServer.java
@@ -49,6 +49,7 @@ public class FilefinderServer implements Runnable {
 
     private void interpretPacket(DatagramPacket packet, ArrayList<InetAddress> addresses) throws IOException {
         String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println(received);
         String[] strArray = received.split(";");
         if(!strArray[0].equals("s") || Integer.parseInt(strArray[3]) > 0){
             return;
