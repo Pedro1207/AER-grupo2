a7ee2e630c4458157b74b4735df2dc0036d58fa2
Nelson Faria <49026704+nelson31@users.noreply.github.com>
2021-05-24 17:46:47 +0100

    inicio view

---
+++ Java/View.java
@@ -0,0 +1,23 @@
+public class View {
+
+    public static void menu(){
+
+        System.out.println("\n***********************************************************");
+        System.out.println("************************P2P-NETWORK************************");
+        System.out.println("\n***************************MENU**************************\n");
+
+        System.out.println("[1] -> Pesquisa por ficheiros ");
+        System.out.println("[2] -> Sair \n");
+
+        System.out.print("Insira a opção: ");
+    }
+
+    public static void menuPesquisa(){
+
+        System.out.println("\n***********************************************************");
+        System.out.println("************************P2P-NETWORK************************");
+        System.out.println("\n*************************PESQUISA*************************\n");
+
+        System.out.print("Insira o termo a pesquisar: ");
+    }
+}
